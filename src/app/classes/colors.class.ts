17488541ec9b703b7a9f44674220e44f9d802a8c
André Lizarán <113467726+andrelizarandev@users.noreply.github.com>
2024-02-10 20:12:09 -0600

    Update colors.class.ts

--- src/app/classes/colors.class.ts
+++ src/app/classes/colors.class.ts
@@ -1,19 +1,19 @@
 export default class ColorsClasses {
 
   // Primary
-  public static textPrimaryColor = 'text-blue-600';
+  public static textPrimaryColor = 'text-blue-700';
 
-  public static textDarkerPrimaryColor = 'text-blue-700';
+  public static textDarkerPrimaryColor = 'text-blue-800';
 
-  public static bgPrimaryColor = 'bg-blue-600';
+  public static bgPrimaryColor = 'bg-blue-700';
 
-  public static bgDarkerPrimaryColor = 'bg-blue-700';
+  public static bgDarkerPrimaryColor = 'bg-blue-800';
 
-  public static bgDarkerPrimaryColorHover = 'hover:bg-blue-700';
+  public static bgDarkerPrimaryColorHover = 'hover:bg-blue-800';
 
-  public static borderPrimaryColor = 'border-blue-600';
+  public static borderPrimaryColor = 'border-blue-700';
 
-  public static borderDarkerPrimaryColor = 'border-blue-700';
+  public static borderDarkerPrimaryColor = 'border-blue-800';
 
   // Secondary
   public static bgColor = 'bg-neutral-800';
